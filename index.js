8c0fb702b4dd682c3f207f401b0d8a159b941331
yuki-hinata <bigisland20001010@gmail.com>
2025-10-13 18:30:48 +0900

    fix: terminal上で押されたkeyにより、出すものを変える

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-import inquirer from 'inquirer';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 
 async function main() {
-  const answer = await inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
-    },
-  ]);
-  console.log(answer);
+  const rl = readline.createInterface({
+    input,
+    output,
+  });
+
+  const result = await rl.question("操作を選択して下さい: [N]名前検索 [Y]勤続年数 [Q]システム終了");
+  if (result === "N") {
+    console.log("名前検索を実行します。");
+  } else if (result === "Y") {
+    console.log("勤続年数を実行します。");
+  } else if (result === "Q") {
+    console.log("システムを終了します。");
+    rl.close();
+  }
 }
 
 main();
