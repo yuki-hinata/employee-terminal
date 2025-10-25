08f1389794d87c8827c98d046129f582474344c8
yuki-hinata <bigisland20001010@gmail.com>
2025-10-25 22:49:13 +0900

    fix: like検索できるようにする / 繰り返し入力できる

--- index.ts
+++ index.ts
@@ -4,33 +4,45 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function main() {
+// 再帰関数を使って、ユーザーがQやexitで抜けたとき以外は終了しないように実装する
+// これだと実行回数分、入力した文が表示される
+const all = async () => {
   const rl = readline.createInterface({
     input,
     output,
+    prompt: "操作を選択して下さい: [N]名前検索 [Y]勤続年数 [Q]システム終了\n",
   });
 
-  const result = await rl.question("操作を選択して下さい: [N]名前検索 [Y]勤続年数 [Q]システム終了\n");
-  if (result === "N") {
-    const employee = await prisma.employee.findMany();
-    console.log(employee);
-    console.log("名前検索を実行します。");
-  } else if (result === "Y") {
-    console.log("勤続年数を実行します。");
-  } else if (result === "Q") {
-    console.log("システムを終了します。");
-    rl.close();
-  } else {
-    console.log("無効な操作です。");
-    rl.close();
-  }
+  rl.prompt();
+
+  rl.on("line", async (line) => {
+    switch (line) {
+      case "N":
+        const name = await rl.question("名前を入力して下さい: ");
+        const employee = await prisma.employee.findFirst({
+          where: {
+            name: { contains: name },
+          },
+        });
+        console.log(employee ?? "該当者がいません。");
+        rl.prompt();
+        break;
+      case "Q":
+        console.log("システムを終了します。");
+        rl.close();
+        break;
+      default:
+        console.log("該当するキーバインドがありません。再度入力してください。");
+        rl.prompt();
+    }
+  });
+}
+
+async function main() {
+  await all();
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-    console.log("プログラムを終了します。");
-  })
   .catch(async (error) => {
     console.error(error);
     await prisma.$disconnect();
