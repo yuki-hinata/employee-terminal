49280f57397be1448690e82c93e2a33881f17875
yuki-hinata <bigisland20001010@gmail.com>
2025-10-26 17:27:00 +0900

    fix: SET TTL

--- command/searchEmployeeByName.ts
+++ command/searchEmployeeByName.ts
@@ -13,37 +13,41 @@ type Props = {
 export const searchEmployeeByName = async ({ rl, redis, prisma }: Props) => {
   const name = await rl.question("名前を入力して下さい: ");
 
-        if (!name.trim()) {
-          console.log("空文字での入力はできません。再度入力してください。");
-          rl.prompt();
-          return;
-        }
+  if (!name.trim()) {
+    console.log("空文字での入力はできません。再度入力してください。");
+    rl.prompt();
+    return;
+  }
 
-        const escapedName = escapeLikePattern(name.trim());
+  const escapedName = escapeLikePattern(name.trim());
 
-        const cachedEmployees = await redis.get(`name_search_cache:${escapedName}`);
-        if (cachedEmployees) {
-          console.log(JSON.parse(cachedEmployees).map(formatEmployee).join("\n"));
-          rl.prompt();
-          return;
-        }
+  const cachedEmployees = await redis.get(`name_search_cache:${escapedName}`);
+  if (cachedEmployees) {
+    console.log(JSON.parse(cachedEmployees).map(formatEmployee).join("\n"));
+    rl.prompt();
+    return;
+  }
 
-        const employee = await prisma.employee.findMany({
-          select: {
-            id: true,
-            name: true,
-            yearsOfService: true,
-            position: true,
-          },
-          where: {
-            name: { contains: escapedName },
-          },
-        });
+  const employee = await prisma.employee.findMany({
+    select: {
+      id: true,
+      name: true,
+      yearsOfService: true,
+      position: true,
+    },
+    where: {
+      name: { contains: escapedName },
+    },
+  });
 
-        if (employee.length > 0) {
-          await redis.set(`name_search_cache:${escapedName}`, JSON.stringify(employee));
-        }
+  if (employee.length > 0) {
+    await redis.set(`name_search_cache:${escapedName}`, JSON.stringify(employee), {
+      // 社員の更新頻度はそこまで高くないと仮定して、30分キャッシュする
+      EX: 1800,
+      NX: true
+    });
+  }
 
-        console.log(employee.length > 0 ? employee.map(formatEmployee).join("\n") : "該当者がいません。");
-        rl.prompt();
+  console.log(employee.length > 0 ? employee.map(formatEmployee).join("\n") : "該当者がいません。");
+  rl.prompt();
 }
