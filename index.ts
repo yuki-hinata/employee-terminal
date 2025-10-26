d9042dc3773308783a81da53cf96b9164a2fe504
yuki-hinata <bigisland20001010@gmail.com>
2025-10-26 12:58:53 +0900

    chore: Redisを導入して、一度検索した条件はRedisから取得し、DBにアクセスいかないようにする

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 import { PrismaClient, type Employee } from '@prisma/client';
+import { createClient } from 'redis';
 
 const prisma = new PrismaClient();
 
@@ -16,6 +17,10 @@ function formatEmployee(employee: Omit<Employee, "createdAt" | "updatedAt">): st
 }
 
 async function main() {
+  const redis = await createClient({
+    url: 'redis://redis:6379'
+  }).on('error', (err) => console.error('Redis Error', err)).connect();
+
   const rl = readline.createInterface({
     input,
     output,
@@ -36,6 +41,15 @@ async function main() {
         }
 
         const escapedName = escapeLikePattern(name.trim());
+
+        const cachedEmployees = await redis.get(`name_search_cache:${escapedName}`);
+        if (cachedEmployees) {
+          console.log('キャッシュから取得しました。');
+          console.log(JSON.parse(cachedEmployees));
+          rl.prompt();
+          return;
+        }
+
         const employee = await prisma.employee.findMany({
           select: {
             id: true,
@@ -47,26 +61,13 @@ async function main() {
             name: { contains: escapedName },
           },
         });
-        console.log(employee.length > 0 ? employee.map(formatEmployee).join("\n") : "該当者がいません。");
-        rl.prompt();
-        break;
-      case "Y":
-        const yearsOfService = await rl.question("絞り込みたい勤続年数を入力して下さい(x年以上): ");
-        if (!yearsOfService.trim()) {
-          console.log("空文字での入力はできません。再度入力してください。");
-          rl.prompt();
-          return;
+
+        if (employee.length > 0) {
+          console.log('キャッシュに保存しました。');
+          await redis.set(`name_search_cache:${escapedName}`, JSON.stringify(employee));
         }
-        const employees = await prisma.employee.findMany({
-          select: {
-            id: true,
-            name: true,
-            yearsOfService: true,
-            position: true,
-          },
-          where: { yearsOfService: { gte: parseInt(yearsOfService.trim()) } },
-        });
-        console.log(employees.length > 0 ? employees.map(formatEmployee).join("\n") : "該当者がいません。");
+
+        console.log(employee.length > 0 ? employee.map(formatEmployee).join("\n") : "該当者がいません。");
         rl.prompt();
         break;
       case "Q":
